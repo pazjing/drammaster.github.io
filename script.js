953a8825c355e0645aaaf857517132fba0ad58c4
Drammaster <Csabi1996@windowslive.com>
2020-06-15 23:57:44 +1200

    Color Changer
    Figured out how to change CSS using JS

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/*Function to change a text field*/
 function myFunction () {
     document.getElementById("mainfield").innerHTML = "You clicked on the Link"
 };
+
+/*Function to change #mainfield to random colors*/
+function colorChange() {
+    var mainfield = document.querySelector('#mainfield');
+    mainfield.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+};
+
+/*Function to generate Hex*/
+function changeRandomColor () {
+    var newHex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    document.getElementById("mainfield").innerHTML = "It works tho";
+    document.getElementById("mainfield").innerHTML = newHex;
+}
